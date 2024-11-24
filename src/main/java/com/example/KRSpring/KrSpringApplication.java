25ddf06cf54e2617ff1b7ad328cd8ffed0cbfcc1
TTopopo <134698978+TTopopo@users.noreply.github.com>
2024-11-24 21:41:25 +0300

    Add files via upload

---
+++ src/main/java/com/example/KRSpring/KrSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.KRSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/KRSpring/KrSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KRSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
